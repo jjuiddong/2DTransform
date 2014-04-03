52a1c23e978497e2f0b41c726951c6b15708fbb9
jjuiddong <jjuiddong@hanmail.net>
2014-04-03 21:51:02 +0900

    basic render

--- 2DTransform/2DTransform.cpp
+++ 2DTransform/2DTransform.cpp
@@ -4,12 +4,17 @@
 #define MAX_LOADSTRING 100
 
 HINSTANCE hInst;
+HWND g_hWnd;
 TCHAR szTitle[MAX_LOADSTRING];
 TCHAR szWindowClass[MAX_LOADSTRING];
 
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+void					MainLoop(int elapse_time);
+void					Render(HWND hWnd);
+void					Paint(HWND hWnd, HDC hdc);
+
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
@@ -33,6 +38,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY2DTRANSFORM));
 
+	int oldT = GetTickCount();
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -40,6 +46,11 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+
+		const int curT = GetTickCount();
+		const int elapseT = min(curT - oldT, 100);
+		oldT = curT;
+		MainLoop(elapseT);	
 	}
 
 	return (int) msg.wParam;
@@ -73,7 +84,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    hInst = hInstance;
 
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   g_hWnd = hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
    if (!hWnd)
@@ -112,6 +123,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
+		Paint(hWnd, ps.hdc);
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
@@ -123,3 +135,58 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
+
+/**
+ @brief 
+ */
+void	MainLoop(int elapse_time)
+{
+	// Move
+	RECT cr;
+	::GetClientRect(g_hWnd, &cr);
+	//for (size_t i=0; i < m_Balls.size(); ++i)
+	//	m_Balls[ i]->Move( cr, m_Balls, elapse_time );
+
+	// Render
+	Render(g_hWnd);
+	::InvalidateRect(g_hWnd, NULL, TRUE);
+}
+
+
+/**
+ @brief 
+ */
+void	Render(HWND hWnd)
+{
+	HDC hdc = GetDC(hWnd);
+	Paint(hWnd, hdc);
+	::ReleaseDC(hWnd, hdc);
+}
+
+
+/**
+ @brief 
+ */
+void Paint(HWND hWnd, HDC hdc)
+{
+	RECT rc;
+	GetClientRect(hWnd, &rc);
+	HDC hdcMem = CreateCompatibleDC(hdc);
+	HBITMAP hbmMem = CreateCompatibleBitmap(hdc, rc.right-rc.left, rc.bottom-rc.top);
+	HBITMAP hbmOld = (HBITMAP)SelectObject(hdcMem, hbmMem);
+	HBRUSH hbrBkGnd = CreateSolidBrush(GetSysColor(COLOR_WINDOW));
+	FillRect(hdcMem, &rc, hbrBkGnd);
+	DeleteObject(hbrBkGnd);
+
+	//for(size_t i=0; i < m_Balls.size(); ++i)
+	//{
+	//	RECT r = m_Balls[ i]->GetRect();
+	//	SelectObject(hdcMem, g_Brush[ m_Balls[ i]->GetColor()] );
+	//	::Ellipse(hdcMem,  r.left, r.top, r.right, r.bottom);
+	//}
+
+	BitBlt(hdc, rc.left, rc.top, rc.right-rc.left, rc.bottom-rc.top, hdcMem, 0, 0, SRCCOPY);
+	SelectObject(hdcMem, hbmOld);
+	DeleteObject(hbmMem);
+	DeleteDC(hdcMem);
+}
